5ac6ac3ad314aa71df5fab521eba0a84cb716d5d
Kijav16 <kijav16@student.sdu.dk>
2018-02-05 09:22:27 +0100

    Initial commit

---
+++ desktop/src/dk/sdu/multipurpose/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dk.sdu.multipurpose.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dk.sdu.multipurpose.Gameselecter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gameselecter(), config);
+	}
+}
